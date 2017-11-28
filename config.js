062b9fcee10d932f43ad2706ed9c337b05a457e9
Alexis Garcia <alexclaustro@gmail.com>
2017-11-27 21:08:36 -0600

    Add bot file. Code refactorization, better and clearer code

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'
 var config = {
     port: process.env.port || 9000, //dev env only 
-    pageKey:process.env.pageKey || 'EAAES8Cd3PxsBAM2Hj9tIOTS6jZBhZB94dh1Ys0tdixkzHQK9iNKYxGrRe6BMIEKv22BHagZCJ8lZBaZB6mMOZCLRiqySO9r60S1Bl7ASeykyKo8mjpuu2BZBfvALY0ZAZB8fZC7NPZCKtXZCnLKVJP5zXgvQaMvNH4KGYfpxLbdouFmmptXLuVpTDtrB',
+    pageKey:process.env.pageKey || 'EAAHSZClmIeC4BALKCELUavbhGdZAXRyvjkJ2b3rSTQty41P8fopotWQYm72nZCmdJxKjPgDVdxDN53o0tZBV9o7rLrZC3GGdZCDSwkGUYqZBgcmnE4ZBlrI4iLEMBqapar0B8Huf5WIaYlqBim1ktH98Ckx7h5y4PKgfLmqkSKU1QPnfAblReBHa',
     tokenMsn: 'my_token_test_bot',
     weatherKey: process.env.weatherKey || 'f7b011bd1f611507e8454fe16bb4f3ec' 
 };
